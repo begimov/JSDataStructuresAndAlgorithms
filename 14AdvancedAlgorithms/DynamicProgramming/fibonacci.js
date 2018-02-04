cf177d82ba380d2856527294359f27d4a10c9982
begimov <begimov@gmail.com>
2018-02-04 12:07:37 +0300

    dynamic solution to computing Fibonacci numbers implemented

--- 14AdvancedAlgorithms/DynamicProgramming/fibonacci.js
+++ 14AdvancedAlgorithms/DynamicProgramming/fibonacci.js
@@ -4,3 +4,21 @@ function recurFib(n) {
     }
     return recurFib(n - 1) + recurFib(n - 2);
 }
+
+function dynFib(n) {
+    if (n == 0) return 0;
+
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    
+    var val = [];
+    val[1] = 1;
+    val[2] = 1;
+
+    for (var i = 3; i <= n; i++) {
+        val[i] = val[i - 1] + val[i - 2];
+    }
+
+    return val[n];
+}
