aaecbf2540902758593d3ee8177bfedbdfc024e6
begimov <begimov@gmail.com>
2018-02-04 13:20:43 +0300

    basic has function created in HashTable

--- 8_Hashing/HashTableClass.js
+++ 8_Hashing/HashTableClass.js
@@ -10,3 +10,11 @@ function put(data) {
     var pos = this.simpleHash(data);
     this.table[pos] = data;
 }
+
+function simpleHash(data) {
+    var total = 0;
+    for (var i = 0; i < data.length; i++) {
+        total += data.charCodeAt(i);
+    }
+    return total % this.table.length;
+}
