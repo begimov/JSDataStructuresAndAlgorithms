b24366cba20b98070556736dbb8656b341250c53
begimov <begimov@gmail.com>
2018-02-09 15:51:51 +0300

    functions to traverse a list implemented

--- 3_Lists/List.js
+++ 3_Lists/List.js
@@ -2,22 +2,22 @@ function List() {
     this.listSize = 0;
     this.pos = 0;
     this.dataStore = []; // array to store list elements
-    // this.clear = clear;
+    this.clear = clear;
     this.find = find;
     this.toString = toString;
     this.insert = insert;
     this.append = append;
     this.remove = remove;
-    // this.front = front;
-    // this.end = end;
-    // this.prev = prev;
-    // this.next = next;
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
     this.length = length;
-    // this.currPos = currPos;
-    // this.moveTo = moveTo;
-    // this.getElement = getElement;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
     this.length = length;
-    // this.contains = contains;
+    this.contains = contains;
 }
 
 function append(element) {
@@ -76,4 +76,39 @@ function contains(element) {
         }
     }
     return false;
+}
+
+function front() {
+    this.pos = 0;
+}
+
+function end(params) {
+    this.pos = this.listSize - 1;
+}
+
+function prev() {
+    if (this.pos > 0) {
+        --this.pos;
+    }
+}
+
+function next() {
+    if (this.pos < this.listSize - 1) {
+        ++this.pos;
+    }
+}
+
+function currPos() {
+    return this.pos;
+}
+
+function moveTo(position) {
+    if (position >= 0 && position <= this.listSize - 1) {
+        this.pos = position;
+    }
+    return false;
+}
+
+function getElement() {
+    return this.dataStore[this.pos];
 }
